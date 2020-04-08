81000dcea5535a9556674a8348c4dc5ca34dd53d
barnardmutezo@gmail.com <barnardmutezo@gmail.com>
2020-04-08 13:21:45 +0200

    small changes

--- controlers/image.js
+++ controlers/image.js
@@ -1,3 +1,14 @@
+const Clarifai = require("clarifai")
+const app = new Clarifai.App({
+    apiKey: '3901826b10de4d53974fccdc08a6e1fd'
+});
+
+const handleAPI = (req, res) => {
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => { res.json(data) })
+        .catch(err => res.status(400).json('cannot work with API'))
+}
+
 const handleImage = (req, res, db) => {
     const { id } = req.body
     db('users').where('id', '=', id)
@@ -10,5 +21,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-    handleImage
+    handleImage,
+    handleAPI
 }
--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const bodyParser = require("body-parser")
 const bcrypt = require("bcrypt-nodejs")
 const cors = require("cors")
 const knex = require('knex')
-
-
 const register = require('./controlers/register')
 const signIn = require('./controlers/signIn')
 const profile = require('./controlers/profile')
@@ -22,16 +20,13 @@ const db = knex({
     }
 });
 app.use(bodyParser.json())
-
 app.use(cors())
-
+app.get('/', (req, res) => { res.send(database.users) })
 app.post("/signin", signIn.handleSignin(db, bcrypt))
-
 app.get("/profile/:id", (req, res, db) => { profile.getProfile(req, res, db) })
-
 app.put("/image", (req, res) => { image.handleImage(req, res, db) })
-
 app.post("/register", (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.post('/imageurl', (req, res) => { image.handleAPI(req, res) })
 
 const PORT = process.env.PORT
 app.listen(PORT || 3000, () => {
